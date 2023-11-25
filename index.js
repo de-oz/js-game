0b1d6b2aab83c463586b6f79813fdd69e00587d5
de-oz <101826623+de-oz@users.noreply.github.com>
2023-11-25 17:41:57 +0300

    Разместить комнаты

--- index.js
+++ index.js
@@ -22,17 +22,40 @@ for (var i = 0; i < FIELD_ROWS; i++) {
     }
 }
 
-for (var i = 0; i < map.length; i++) {
-    for (var j = 0; j < map[i].length; j++) {
-        var tile = document.createElement("div");
-        tile.className = map[i][j];
+function fieldRender() {
+    field.innerHTML = "";
 
-        var topOffset = i * 26 + "px";
-        var leftOffset = j * 26 + "px";
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            var tile = document.createElement("div");
+            tile.className = map[i][j];
 
-        tile.style.top = topOffset;
-        tile.style.left = leftOffset;
+            var topOffset = i * 26 + "px";
+            var leftOffset = j * 26 + "px";
 
-        field.appendChild(tile);
+            tile.style.top = topOffset;
+            tile.style.left = leftOffset;
+
+            field.appendChild(tile);
+        }
     }
 }
+
+fieldRender();
+
+var roomsNum = Math.floor(Math.random() * 6) + 5;
+
+for (var r = 0; r < roomsNum; r++) {
+    var roomWidth = Math.floor(Math.random() * 6) + 3;
+    var roomHeight = Math.floor(Math.random() * 6) + 3;
+    var originX = Math.floor(Math.random() * (map[0].length - roomWidth + 1));
+    var originY = Math.floor(Math.random() * (map.length - roomHeight + 1));
+
+    for (var y = originY; y < originY + roomHeight; y++) {
+        for (var x = originX; x < originX + roomWidth; x++) {
+            map[y][x] = tileClass;
+        }
+    }
+}
+
+fieldRender();
