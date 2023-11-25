7dae2cb12a2e501db1613f7e0f6fb1b0a7dcbf9b
de-oz <101826623+de-oz@users.noreply.github.com>
2023-11-25 15:16:51 +0300

    Залить всю карту стеной

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 var FIELD_ROWS = 24;
 var FIELD_COLUMNS = 40;
 
-var field = document.querySelector('.field');
+var field = document.querySelector(".field");
 
-var tileClass = 'tile';
-var playerClass = 'tile tileE';
-var hpClass = 'tile tileHP';
-var enemyClass = 'tile tileP';
-var weaponClass = 'tile tileSW';
-var wallClass = 'tile tileW';
+var tileClass = "tile";
+var playerClass = "tile tileE";
+var hpClass = "tile tileHP";
+var enemyClass = "tile tileP";
+var weaponClass = "tile tileSW";
+var wallClass = "tile tileW";
 
 var map = new Array(FIELD_ROWS);
 
@@ -19,5 +19,20 @@ for (var i = 0; i < FIELD_ROWS; i++) {
 
     for (var j = 0; j < FIELD_COLUMNS; j++) {
         map[i][j] = wallClass;
+    }
+}
+
+for (var i = 0; i < map.length; i++) {
+    for (var j = 0; j < map[i].length; j++) {
+        var tile = document.createElement("div");
+        tile.className = map[i][j];
+
+        var topOffset = i * 26 + "px";
+        var leftOffset = j * 26 + "px";
+
+        tile.style.top = topOffset;
+        tile.style.left = leftOffset;
+
+        field.appendChild(tile);
     }
 }
